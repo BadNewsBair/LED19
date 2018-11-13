6a79e093c6cba3b2839248480493614d9a768ab9
Chris Bair <chrisbair5@gmail.com>
2018-11-13 16:56:59 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 import sys
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QHBoxLayout, QGroupBox, QDialog, QVBoxLayout, QLabel, QTextEdit
+from PyQt5.QtGui import QIcon, QPixmap
+
  
 class MainWindow(QWidget):
     def __init__(self, parent = None):
