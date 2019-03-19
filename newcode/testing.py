8aec291eb3cddebf1328cd05331e0831fa31709b
rubyc88 <christopherruby@u.boisestate.edu>
2019-03-18 19:24:10 -0600

    split everything up into functions

--- newcode/testing.py
+++ newcode/testing.py
@@ -17,8 +17,7 @@ class Data():
             rho_theta = pd.read_excel(csv_file,header= None,skiprows= 15)
             #deletes the last 2 columns since they are nans.....probably dont need this when creating our own
             rho_theta = rho_theta.drop(rho_theta.columns[[-2,-1]], axis =1)
-            rho_T=theta = rho_theta.head(2)
-            #_, cols = Rho_Theta.shape
+            rho_theta = rho_theta.head(2)
 
             return rho_theta
         except:
@@ -35,7 +34,6 @@ class Data():
         except :
             print('This didnt work idiot')
 
-    # need to make data rows more universal(espessially without the csv) 
     def data_average(self,data_frame=None):
         data_rows = data_frame.shape[0]
         data_average = data_frame.append(data_frame.mean(axis=0),ignore_index=True)
@@ -51,65 +49,52 @@ class Data():
             right_equation = (2 * math.pi * (1 - math.cos(rho_0 * math.pi/180)))
             return(left_equation-right_equation)
 
-    def Steradains(self, Rho_Theta_data_frame = self):
+    # Creates the Steradians Row and appends to Data Frame
+    def steradains_df(self, Rho_Theta_df, data_df):
         steradians_values = []
         steradians_values.append('Steradains')
         #this is only for for the first value can probably change my code to make prettier
         steradians_values.append(0)
+        total_steradians = 0
+        for i in range (1,Rho_Theta_df.shape[1]-1):
+            rho_0, _  = Rho_Theta_df[i]
+            rho_1, _ = Rho_Theta_df[i+1]    
+            steradians_values.append(self.steradians_equation(rho_0=rho_0, rho_1 =rho_1))
+            total_steradians += self.steradians_equation(rho_0=rho_0, rho_1 = rho_1)
+        steradians_values = pd.Series(dict(zip(data_df.columns, steradians_values)))
+        data = data_df.append(steradians_values, ignore_index = True)
+        return(data , total_steradians)
 
 
+    def lumens(self,data):           
+        lumens_values = []
+        lumens_values.append('Lumens')
+        total_lumens =0
+        for i in range (1, data.shape[1] -1):
+            Average_Row = data[i][data.shape[0]-2]
+            Steradains_Row = data[i][data.shape[0]-1]
+            lumens_values.append(Average_Row*Steradains_Row)
+            total_lumens += Average_Row * Steradains_Row
+        lumens_values = pd.Series(dict(zip(data.columns, lumens_values)))
+        data = data.append(lumens_values, ignore_index = True)
+        return(data.round(3))
 
-        def lumens(self,last_angle_measured):
-
-
-
-
-
-
-
-            
-
-            total_steradians = 0 
-            for i in range (1,cols-1):
-                rho_0, _  = Rho_Theta[i]
-                rho_1, _ = Rho_Theta[i+1]    
-                steradians_values.append(steradians(rho_0=rho_0, rho_1 =rho_1))
-                total_steradians += steradians(rho_0=rho_0, rho_1 = rho_1)
-            print(total_steradians)
-            steradians_values = pd.Series(dict(zip(data.columns, steradians_values)))
-            data = data.append(steradians_values, ignore_index = True)
-
-            lumens_values = []
-            lumens_values.append('Lumens')
-            total_lumens =0
-
-            for i in range (1, cols -1):
-                Average_Row = data[i][data.shape[0]-2]
-                Steradains_Row = data[i][data.shape[0]-1]
-                lumens_values.append(Average_Row*Steradains_Row)
-                total_lumens += Average_Row * Steradains_Row
-
-            lumens_values = pd.Series(dict(zip(data.columns, lumens_values)))
-            data = data.append(lumens_values, ignore_index = True)
-
-            print(total_lumens)
-            #print(data.round(3))
     
 # This is for testing purposes only at the moment
 def main():
-    #Data.am_i_using_a_csv = True
     if Data.am_i_using_a_csv == True:
         try:
             test_data = Data()
-            rho_theta_data,rho_theta_cols = test_data.read_rho_theta_csv(Data.test_csv)
+            rho_theta_data = test_data.read_rho_theta_csv(Data.test_csv)
             data_df, num_rows = test_data.data_csv(Data.test_csv, last_measured_angle=90)
-            average = test_data.data_average(data_df)
-            print(average)
+            
         except:
-            print('im DOING SOMETHING WRONG')
+            print('Something is wrong with the csv')
             exit()
-    print('This might be working')
-    
-    #lumens(90)
+        average = test_data.data_average(data_df)
+        steradains_df, summed_total_steradains = test_data.steradains_df(Rho_Theta_df=rho_theta_data ,data_df=average)
+        data_frame = test_data.lumens(steradains_df)
+        print(data_frame)
+        
 
 main()
